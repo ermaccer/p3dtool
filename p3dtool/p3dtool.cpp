b45bbb927a49b628464c49e9126e70dc9a156217
ermaccer <40604575+ermaccer@users.noreply.github.com>
2022-02-12 13:50:06 +0100

    extract images

--- p3dtool/p3dtool.cpp
+++ p3dtool/p3dtool.cpp
@@ -18,6 +18,10 @@ int main(int argc, char* argv[])
         return 1;
     }
     std::string cmd = argv[1];
+    bool convert = false;
+
+    if (argv[2])
+        convert = strcmp(argv[2], "convert") == 0;
 
     std::string input = cmd.substr(0, cmd.find_first_of(".", 0));
 
@@ -26,59 +30,157 @@ int main(int argc, char* argv[])
     {
         if (std::filesystem::is_directory(cmd))
         {
-            std::string output = cmd;
-            output += ".p3d";
-            std::ofstream oFile(output , std::ofstream::binary);
-
-           // std::filesystem::current_path(cmd);
-
-            std::vector<std::string> filePaths;
-            std::vector<int> fileSizes;
-
-            for (const auto& file : std::filesystem::recursive_directory_iterator(cmd))
+            if (convert)
             {
-                if (file.path().has_extension())
+                std::vector<std::string> filePaths;
+                std::vector<int> fileSizes;
+
+                for (const auto& file : std::filesystem::recursive_directory_iterator(cmd))
                 {
-                    if (file.path().extension().string() == ".p3d_chunk")
+                    if (file.path().has_extension())
                     {
-                        fileSizes.push_back(std::filesystem::file_size(file.path()));
-                        filePaths.push_back(file.path().string());
+                        if (file.path().extension().string() == ".p3d_chunk")
+                        {
+                            fileSizes.push_back(std::filesystem::file_size(file.path()));
+                            filePaths.push_back(file.path().string());
+                        }
                     }
                 }
-            }
 
-            p3d_header header;
-            header.header = 0xFF443350;
-            header.version = 12;
+                int processed = 0;
+                int failed = 0;
 
-            int totalSize = 0;
-            for (unsigned int i = 0; i < fileSizes.size(); i++)
-                totalSize += fileSizes[i];
-            totalSize += sizeof(p3d_header);
-            header.fileSize = totalSize;
-
-            oFile.write((char*)&header, sizeof(p3d_header));
-
-            for (unsigned int i = 0; i < filePaths.size(); i++)
-            {
-                std::ifstream pFile(filePaths[i], std::ifstream::binary);
-                std::cout << "Processing: " << filePaths[i] << std::endl;
-
-                if (!pFile)
+                for (unsigned int i = 0; i < filePaths.size(); i++)
                 {
-                    std::cout << "ERROR: Could not open " << filePaths[i] << "!" << std::endl;
-                    return 1;
+                    std::ifstream pFile(filePaths[i], std::ifstream::binary);
+                    if (!pFile)
+                    {
+                        std::cout << "ERROR: Could not open " << filePaths[i] << "!" << std::endl;
+                        return 1;
+                    }
+
+                    p3d_chunk chunk;
+                    pFile.read((char*)&chunk,sizeof(p3d_chunk));
+                    std::string folder_name = filePaths[i].substr(0, filePaths[i].find_first_of(".", 0));
+
+                    std::filesystem::path originalPath = std::filesystem::current_path();
+
+                    if (chunk.id == CHUNK_TEXTURE)
+                    {
+                        std::filesystem::create_directory(folder_name);
+                        std::filesystem::current_path(folder_name);
+
+                        char nameLen;
+                        pFile.read((char*)&nameLen, sizeof(char));
+                        std::unique_ptr<char[]> strData = std::make_unique<char[]>(nameLen + 1);
+                        pFile.read(strData.get(), nameLen);
+
+                        std::string textureName = &strData[0];
+
+                        std::cout << "Processing texture " << textureName << std::endl;
+                        p3d_texture_header tex;
+                        pFile.read((char*)&tex, sizeof(p3d_texture_header));
+                        processed++;
+
+
+                        for (int i = 0; i < 2; i++)
+                        {
+                            p3d_chunk next;
+                            pFile.read((char*)&next, sizeof(p3d_chunk));
+
+                            if (next.id == CHUNK_IMAGE)
+                            {
+                                char nameLen;
+                                pFile.read((char*)&nameLen, sizeof(char));
+                                std::unique_ptr<char[]> strData = std::make_unique<char[]>(nameLen + 1);
+                                pFile.read(strData.get(), nameLen);
+
+                                std::string textureName = &strData[0];
+                                p3d_image_header img;
+                                pFile.read((char*)&img, sizeof(p3d_image_header));
+
+                            }
+                            else if (next.id == CHUNK_IMAGEDATA)
+                            {
+                                int size;
+                                pFile.read((char*)&size, sizeof(int));
+                                std::unique_ptr<char[]> imgData = std::make_unique<char[]>(size);
+                                pFile.read(imgData.get(), size);
+
+                                std::string output = textureName;
+                                output += ".png";
+                                std::ofstream oFile(output, std::ofstream::binary);
+                                oFile.write(imgData.get(), size);
+                                std::cout << "Saved image '" << output << "'" << std::endl;
+
+                            }
+                        }
+                        std::filesystem::current_path(originalPath);
+
+                    }
+                    else
+                        failed++;
+
+                }
+                std::cout << "Procesesd " << processed << " chunks and failed to process " << failed << std::endl;
+                std::cout << "Finished." << std::endl;
+            }
+            else
+            {
+                std::string output = cmd;
+                output += ".p3d";
+                std::ofstream oFile(output, std::ofstream::binary);
+
+
+                std::vector<std::string> filePaths;
+                std::vector<int> fileSizes;
+
+                for (const auto& file : std::filesystem::recursive_directory_iterator(cmd))
+                {
+                    if (file.path().has_extension())
+                    {
+                        if (file.path().extension().string() == ".p3d_chunk")
+                        {
+                            fileSizes.push_back(std::filesystem::file_size(file.path()));
+                            filePaths.push_back(file.path().string());
+                        }
+                    }
                 }
 
-                int chunkSize = fileSizes[i];
-                std::unique_ptr<char[]> chunkData = std::make_unique<char[]>(chunkSize);
-                pFile.read(chunkData.get(), chunkSize);
+                p3d_header header;
+                header.header = 0xFF443350;
+                header.version = 12;
 
-                oFile.write(chunkData.get(), chunkSize);
+                int totalSize = 0;
+                for (unsigned int i = 0; i < fileSizes.size(); i++)
+                    totalSize += fileSizes[i];
+                totalSize += sizeof(p3d_header);
+                header.fileSize = totalSize;
 
+                oFile.write((char*)&header, sizeof(p3d_header));
+
+                for (unsigned int i = 0; i < filePaths.size(); i++)
+                {
+                    std::ifstream pFile(filePaths[i], std::ifstream::binary);
+                    std::cout << "Processing: " << filePaths[i] << std::endl;
+
+                    if (!pFile)
+                    {
+                        std::cout << "ERROR: Could not open " << filePaths[i] << "!" << std::endl;
+                        return 1;
+                    }
+
+                    int chunkSize = fileSizes[i];
+                    std::unique_ptr<char[]> chunkData = std::make_unique<char[]>(chunkSize);
+                    pFile.read(chunkData.get(), chunkSize);
+
+                    oFile.write(chunkData.get(), chunkSize);
+
+                }
+                std::cout << "Built '" << output << "'!" << std::endl;
+                std::cout << "Finished." << std::endl;
             }
-            std::cout << "Built '" << output << "'!" << std::endl;
-            std::cout << "Finished." << std::endl;
+            
         }
         else
         {
--- p3dtool/pure3d.h
+++ p3dtool/pure3d.h
@@ -11,3 +11,30 @@ struct p3d_chunk {
 	int dataSize;
 	int size;
 };
+
+enum p3d_chunks {
+	CHUNK_TEXTURE = 0x19000,
+	CHUNK_IMAGE = 0x19001,
+	CHUNK_IMAGEDATA = 0x19002,
+};
+
+struct p3d_texture_header {
+	int unk;
+	int x;
+	int y;
+	int bits;
+	int alphaDepth;
+	int mipMaps;
+	int type;
+	int usage;
+	int priority;
+};
+struct p3d_image_header {
+	int unk;
+	int x;
+	int y;
+	int bits;
+	int pal;
+	int alpha;
+	int format;
+};
